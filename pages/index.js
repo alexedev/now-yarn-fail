dd8fe02ea9026882bd0c7212d864cfa0ed1c75dd
Alex Alexeev <alex.alexeev@kiwi.com>
2019-01-07 17:12:48 +0100

    Simplify

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
-// @flow
-
 import React from 'react';
 
-import dynamic from 'next/dynamic';
-const App = dynamic(() => import('../src/components/App'), {
-  ssr: false
-});
-
-const Index = () => <App />;
+const Index = () => 'test';
 
 export default Index;
